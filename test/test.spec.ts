afb0132a7c164427cabc9345fe9e482b1bdca399
canbax <yusufsaidcanbaz@gmail.com>
2024-03-07 15:40:53 +0300

    upgrade all dependencies, use lint-staged

--- test/test.spec.ts
+++ test/test.spec.ts
@@ -1,7 +1,8 @@
 import { yourLibFunction } from "../src/main";
+import { describe, test, expect } from "vitest";
 
 describe("Unit tests", () => {
-  it("should add 2 and 5", async () => {
+  test("should add 2 and 5", async () => {
     expect(yourLibFunction(2, 5)).toEqual(7);
   });
 });
