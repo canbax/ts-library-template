282d11861843ee5606f7d49e632ead396692e7e3
canbax <yusufsaidcanbaz@gmail.com>
2025-07-19 23:18:00 +0300

    refactor: simplify layout logic and remove unused style.css

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
-import "./style.css";
 import { yourLibFunction } from "./index.ts";
 
-const nodes = ["a", "b", "c"];
-const edges = { a: "b", b: "c", c: "a" };
-const layoutResult = yourLibFunction(nodes, edges);
+const layoutResult = yourLibFunction(1, 2);
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
