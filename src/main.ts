1f1276ce7587a4b635963e7f824f76cb9c4aeeda
canbax <yusufsaidcanbaz@gmail.com>
2025-07-19 23:20:38 +0300

    refactor: remove unused header from main layout

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ const layoutResult = yourLibFunction(1, 2);
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
-    <h1>Discrete Layout</h1>
     <pre>${JSON.stringify(layoutResult, null, 2)}</pre>
   </div>
 `;
